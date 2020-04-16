8b889c9d1e8a639f7bba5320dc50fcc4d2144550
Sid Lucidworks <siddharth@lucidworks.com>
2020-04-17 04:24:37 +0530

    chore: move config.php to project root

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = array(
+    'endpoint' => array(
+        'localhost' => array(
+            'scheme' => 'https', # or https
+            'host' => 'pg01.us-west1.cloud.lucidworks.com',
+            'port' => 443,
+            'path' => '/php-test/php-test/',
+            'core' => 'test',
+            'oauth2_client_id' => '0oa3j6emghW1EvQ9P357',
+            'oauth2_client_secret' => 'WTjGhFCh1z5gntC38WM5d36xe1kAtOw8hh-Kdv0y',
+        )
+    )
+);
